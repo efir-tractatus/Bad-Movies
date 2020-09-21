9377ae9410c9252acef43542498665e52a65fe6b
Denis Sanches <denis.a.sanches@gmail.com>
2020-09-20 22:16:11 -0400

    implement saveMovie function

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -4,11 +4,10 @@ const apiHelpers = require('../helpers/apiHelpers.js');
 //Return requests to the client
 module.exports = {
   getSearch: (req, res) => {
-    var genre_id = req.query.id
+    var genre_id = req.query.id;
     apiHelpers
       .discoverByGenre(genre_id)
       .then((response) => {
-        console.log('Controllers', response)
         res.status(200).send(response.data);
       })
       .catch((err) => {
@@ -25,6 +24,17 @@ module.exports = {
         res.status(500).send(err);
       });
   },
-  saveMovie: (req, res) => {},
+  saveMovie: (req, res) => {
+    var movieObj = req.body;
+    movieModel
+      .save(movieObj)
+      .then((result) => {
+        res.status(201).send(result);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send(err);
+      });
+  },
   deleteMovie: (req, res) => {},
 };
